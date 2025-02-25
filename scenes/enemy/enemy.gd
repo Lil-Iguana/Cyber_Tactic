416aed96201cae272e59f082391ee0133952389b
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-02-25 17:52:45 +0800

    Completed Part 4
    Added enemy scene and testing connection between StatsUI and Resources

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -1,34 +1,38 @@
-class_name Player
-extends Node2D
+class_name Enemy
+extends Area2D
 
-@export var stats: CharacterStats : set = set_character_stats
+const ARROW_OFFSET := 5
+
+@export var stats: Stats : set = set_enemy_stats
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var arrow: Sprite2D = $Arrow
 @onready var stats_ui: StatsUI = $StatsUI as StatsUI
 
 
 
-func set_character_stats(value: CharacterStats) -> void:
+func set_enemy_stats(value: Stats) -> void:
 	stats = value.create_instance()
 	
 	if not stats.stats_changed.is_connected(update_stats):
 		stats.stats_changed.connect(update_stats)
 	
-	update_player()
+	update_enemy()
 
 
-func update_player() -> void:
-	if not stats is CharacterStats:
+func update_stats() -> void:
+	stats_ui.update_stats(stats)
+
+
+func update_enemy() -> void:
+	if not stats is Stats:
 		return
 	if not is_inside_tree():
 		await ready
 	
 	sprite_2d.texture = stats.art
+	arrow.position = Vector2.RIGHT * (sprite_2d.get_rect().size.x / 2 + ARROW_OFFSET)
 	update_stats()
-
-
-func update_stats() -> void:
-	stats_ui.update_stats(stats)
 
 
 func take_damage(damage: int) -> void:
