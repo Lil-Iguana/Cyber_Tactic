626be728e37852f0cd8a669ff32991f6751de017
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-02-26 20:30:28 +0800

    Card Visual Overhaul Step 4
    Part 5 (20:01)

--- scenes/card_ui/card_ui.gd
+++ scenes/card_ui/card_ui.gd
@@ -3,7 +3,11 @@ extends Control
 
 signal reparent_requested(which_card_ui: CardUI)
 
-@export var card: Card
+const BASE_STYLEBOX := preload("res://scenes/card_ui/card_base_stylebox.tres")
+const DRAG_STYLEBOX := preload("res://scenes/card_ui/card_dragging_stylebox.tres")
+const HOVER_STYLEBOX := preload("res://scenes/card_ui/card_hover_stylebox.tres")
+
+@export var card: Card : set = _set_card
 
 @onready var panel: Panel = $Panel
 @onready var cost: Label = $Cost
@@ -15,25 +19,39 @@ signal reparent_requested(which_card_ui: CardUI)
 var parent: Control
 var tween: Tween
 
+
 func _ready() -> void:
 	card_state_machine.init(self)
 
+
 func _input(event: InputEvent) -> void:
 	card_state_machine.on_input(event)
+
 
 func animate_to_position(new_postion: Vector2, duration: float) -> void:
 	tween = create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "global_position", new_postion, duration)
 
+
 func _on_gui_input(event: InputEvent) -> void:
 	card_state_machine.on_gui_input(event)
+
 
 func _on_mouse_entered() -> void:
 	card_state_machine.on_mouse_entered()
 
+
 func _on_mouse_exited() -> void:
 	card_state_machine.on_mouse_exited()
 
+
+func _set_card(value: Card) -> void:
+	if not is_node_ready():
+		await ready
+	
+	card = value
+	cost.text = str(card.cost)
+	icon.texture = card.icon
 
 func _on_drop_point_detector_area_entered(area: Area2D) -> void:
 	if not targets.has(area):
