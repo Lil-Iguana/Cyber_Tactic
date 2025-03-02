5ea059d6f463dcfee7ece03616895479d9fc84cc
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-03-02 21:03:50 +0800

    Implemented Draw & Discard function
    Part 6 Complete

--- global/events.gd
+++ global/events.gd
@@ -1,13 +1,25 @@
 extends Node
 
 # Card-related events
+@warning_ignore("unused_signal")
 signal card_drag_started(card_ui: CardUI)
+@warning_ignore("unused_signal")
 signal card_drag_ended(card_ui: CardUI)
+@warning_ignore("unused_signal")
 signal card_aim_started(card_ui: CardUI)
+@warning_ignore("unused_signal")
 signal card_aim_ended(card_ui: CardUI)
+@warning_ignore("unused_signal")
 signal card_played(card: Card)
+@warning_ignore("unused_signal")
 signal card_tooltip_requested(card: Card)
+@warning_ignore("unused_signal")
 signal tooltip_hide_requested
 
 # Player-related events
+@warning_ignore("unused_signal")
 signal player_hand_drawn
+@warning_ignore("unused_signal")
+signal player_hand_discarded
+@warning_ignore("unused_signal")
+signal player_turn_ended
