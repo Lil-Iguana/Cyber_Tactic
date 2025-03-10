157ec27cfd7dce849781c2d18712ee7783ff8255
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-03-10 14:31:36 +0800

    Update card_clicked_state.gd
    Fixed bug when player clicked a card, tooltip does not hide when entered.

--- scenes/card_ui/card_states/card_clicked_state.gd
+++ scenes/card_ui/card_states/card_clicked_state.gd
@@ -3,6 +3,7 @@ extends CardState
 func enter() -> void:
 	card_ui.drop_point_detector.monitoring = true
 	card_ui.original_index = card_ui.get_index()
+	Events.tooltip_hide_requested.emit()
 
 
 func on_input(event: InputEvent) -> void:
