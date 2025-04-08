ebb4982408125aee408beee59b2a1adeccccfbe3
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-04-08 16:59:47 +0800

    Update enemy script with Stats
    S2E8 54:59

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -8,8 +8,9 @@ const WHITE_SPRITE_MATERIAL := preload("res://art/white_sprite_material.tres")
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var arrow: Sprite2D = $Arrow
-@onready var stats_ui: StatsUI = $StatsUI as StatsUI
-@onready var intent_ui: IntentUI = $IntentUI as IntentUI
+@onready var stats_ui: StatsUI = $StatsUI
+@onready var intent_ui: IntentUI = $IntentUI
+@onready var status_handler: StatusHandler = $StatusHandler
 
 var enemy_action_picker: EnemyActionPicker
 var current_action: EnemyAction : set = set_current_action
@@ -95,6 +96,7 @@ func take_damage(damage: int) -> void:
 			sprite_2d.material = null
 			
 			if stats.health <= 0:
+				Events.enemy_died.emit(self)
 				queue_free()
 	)
 
