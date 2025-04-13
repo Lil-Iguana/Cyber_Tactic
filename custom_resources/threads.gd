23dde84cbec5a229b19dd265f28da9eaf4a9de17
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-04-13 15:14:57 +0800

    Created Thread Resource Code
    S2E11 12:07

---
+++ custom_resources/threads.gd
@@ -0,0 +1,13 @@
+class_name Threads
+extends Resource
+
+enum Type {START_OF_TURN, START_OF_COMBAT, END_OF_TURN, END_OF_COMBAT, EVENT_BASED}
+enum CharacterType {ALL, ASSASSIN, WARRIOR, WIZARD}
+
+@export var thread_name: String
+@export var id: String
+@export var type: Type
+@export var character_type: CharacterType
+@export var starter_thread: bool = false
+@export var icon: Texture
+@export_multiline var tooltip: String
