15c84314d211b67fd7ab430b1faff72412ffd9bd
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-04-14 20:37:00 +0800

    Update thread_passive.gd
    S2E11 1:08:05

--- custom_resources/thread_passive.gd
+++ custom_resources/thread_passive.gd
@@ -11,3 +11,35 @@ enum CharacterType {ALL, ASSASSIN, WARRIOR, WIZARD}
 @export var starter_thread: bool = false
 @export var icon: Texture
 @export_multiline var tooltip: String
+
+
+func initialize_thread(_owner: ThreadUI) -> void:
+	pass
+
+
+func activate_thread(_owner: ThreadUI) -> void:
+	pass
+
+
+# This method should be implemented by event-based relics
+# which connect to the EventBus to make sure that they are
+# disconnected when a relic gets removed.
+func deactivate_thread(_owner: ThreadUI) -> void:
+	pass
+
+
+func get_tooltip() -> String:
+	return tooltip
+
+
+func can_appear_as_reward(character: CharacterStats) -> bool:
+	if starter_thread:
+		return false
+
+	if character_type == CharacterType.ALL:
+		return true
+		
+	var thread_char_name: String = CharacterType.keys()[character_type].to_lower()
+	var char_name := character.character_name.to_lower()
+	
+	return thread_char_name == char_name
