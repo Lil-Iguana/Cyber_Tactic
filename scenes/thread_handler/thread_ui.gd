aabe5fd59fb3aafc80cff089f2c25e69af2f2738
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-04-14 15:39:49 +0800

    Added temp code
    S2E11 28:48

--- scenes/thread_handler/thread_ui.gd
+++ scenes/thread_handler/thread_ui.gd
@@ -2,8 +2,24 @@ class_name ThreadUI
 extends Control
 
 
-@export var thread: Threads : set = set_thread
+@export var threads: Threads : set = set_thread
+
+@onready var icon: TextureRect = $Icon
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func set_thread(new_thread: Threads) -> void:
+	if not is_node_ready():
+		await ready
+	
+	threads = new_thread
+	icon.texture = threads.icon
+
+
+func flash() -> void:
+	animation_player.play("flash")
 
 
 func _on_gui_input(event: InputEvent) -> void:
-	pass # Replace with function body.
+	if event.is_action_pressed("left_mouse"):
+		print("Thread tooltip")
