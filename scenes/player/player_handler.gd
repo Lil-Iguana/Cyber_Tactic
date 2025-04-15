3c51f666346284256072ec570d7c594535d551ef
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-04-15 18:04:05 +0800

    Added ThreadHandler to PlayerHandler
    S2E11 1:52:22

--- scenes/player/player_handler.gd
+++ scenes/player/player_handler.gd
@@ -1,9 +1,18 @@
+# Player turn order:
+# 1. START_OF_TURN Relics 
+# 2. START_OF_TURN Statuses
+# 3. Draw Hand
+# 4. End Turn 
+# 5. END_OF_TURN Relics 
+# 6. END_OF_TURN Statuses
+# 7. Discard Hand
 class_name PlayerHandler
 extends Node
 
 const HAND_DRAW_INTERVAL := 0.25
 const HAND_DISCARD_INTERVAL := 0.25
 
+@export var threads: ThreadHandler
 @export var player: Player
 @export var hand: Hand
 
@@ -19,6 +28,7 @@ func start_battle(char_stats: CharacterStats) -> void:
 	character.draw_pile = character.deck.duplicate(true)
 	character.draw_pile.shuffle()
 	character.discard = CardPile.new()
+	threads.threads_activated.connect(_on_threads_activated)
 	player.status_handler.statuses_applied.connect(_on_statuses_applied)
 	start_turn()
 
@@ -26,12 +36,12 @@ func start_battle(char_stats: CharacterStats) -> void:
 func start_turn() -> void:
 	character.block = 0
 	character.reset_mana()
-	player.status_handler.apply_statuses_by_type(Status.Type.START_OF_TURN)
+	threads.activate_threads_by_type(ThreadPassive.Type.START_OF_TURN)
 
 
 func end_turn() -> void:
 	hand.disable_hand()
-	player.status_handler.apply_statuses_by_type(Status.Type.END_OF_TURN)
+	threads.activate_threads_by_type(ThreadPassive.Type.END_OF_TURN)
 
 
 func draw_card() -> void:
@@ -91,3 +101,11 @@ func _on_statuses_applied(type: Status.Type) -> void:
 			draw_cards(character.cards_per_turn)
 		Status.Type.END_OF_TURN:
 			discard_cards()
+
+
+func _on_threads_activated(type: ThreadPassive.Type) -> void:
+	match type:
+		ThreadPassive.Type.START_OF_TURN:
+			player.status_handler.apply_statuses_by_type(Status.Type.START_OF_TURN)
+		ThreadPassive.Type.END_OF_TURN:
+			player.status_handler.apply_statuses_by_type(Status.Type.END_OF_TURN)
