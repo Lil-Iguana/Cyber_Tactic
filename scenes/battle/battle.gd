d477f49dba38a30027fbe74bb817c853c8016f96
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-04-15 16:59:44 +0800

    Hooked up Threads System in Battle
    S2E11 1:25:38

--- scenes/battle/battle.gd
+++ scenes/battle/battle.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var battle_stats: BattleStats
 @export var char_stats: CharacterStats
 @export var music: AudioStream
+@export var threads: ThreadHandler
 
 @onready var battle_ui: BattleUI = $BattleUI
 @onready var player_handler: PlayerHandler = $PlayerHandler 
@@ -29,13 +30,13 @@ func start_battle() -> void:
 	enemy_handler.setup_enemies(battle_stats)
 	enemy_handler.reset_enemy_actions()
 	
-	player_handler.start_battle(char_stats)
-	battle_ui.initialize_card_pile_ui()
+	threads.threads_activated.connect(_on_threads_activated)
+	threads.activate_threads_by_type(ThreadPassive.Type.START_OF_COMBAT)
 
 
 func _on_enemies_child_order_changed() -> void:
 	if enemy_handler.get_child_count() == 0:
-		Events.battle_over_screen_requested.emit("Victory!", BattleOverPanel.Type.WIN)
+		threads.activate_threads_by_type(ThreadPassive.Type.END_OF_COMBAT)
 
 
 func _on_enemy_turn_ended() -> void:
@@ -45,3 +46,12 @@ func _on_enemy_turn_ended() -> void:
 
 func _on_player_died() -> void:
 	Events.battle_over_screen_requested.emit("Game Over!", BattleOverPanel.Type.LOSE)
+
+
+func _on_threads_activated(type: ThreadPassive.Type) -> void:
+	match type:
+		ThreadPassive.Type.START_OF_COMBAT:
+			player_handler.start_battle(char_stats)
+			battle_ui.initialize_card_pile_ui()
+		ThreadPassive.Type.END_OF_COMBAT:
+			Events.battle_over_screen_requested.emit("Victorious!", BattleOverPanel.Type.WIN)
